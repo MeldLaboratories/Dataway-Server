5a8f7ed49d3905711599a34b898d1c256de8e698
tensoid <iubaqua0@gmail.com>
2022-05-18 19:59:02 +0200

    updated result codes

--- src/main/java/ob/dataway/communication/formats/ResultCode.java
+++ src/main/java/ob/dataway/communication/formats/ResultCode.java
@@ -6,10 +6,10 @@ public class ResultCode {
 
   public static final int SUCCESS = 0;
   public static final int USER_NOT_FOUND = 1;
-  public static final int RECEIVER_IN_DO_NOT_DISTURB = 2;
-  public static final int RECIEVER_OFFLINE = 3;
-  public static final int RECEIVER_NOT_FOUND = 4;
-  public static final int NOT_LOGGED_IN = 5;
-  public static final int HANDSHAKE_EXPECTED = 6;
+  public static final int RECIEVER_OFFLINE = 2;
+  public static final int RECEIVER_NOT_FOUND = 3;
+  public static final int NOT_LOGGED_IN = 4;
+  public static final int HANDSHAKE_EXPECTED = 5;
+  public static final int SELF_TRANSFER = 6;
   public static final int UNKNOWN_ERROR = 99;
 }
